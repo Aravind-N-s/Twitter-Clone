cd6b8d4aa1cfaec8573a2b8fd28f627146c94e68
Asura <aravind1808@gmail.com>
2020-02-13 20:18:45 +0530

    Final Commit

--- client/src/components/utils/service.js
+++ client/src/components/utils/service.js
@@ -1,19 +1,11 @@
 export const isValid = (field, value) => {
   const emailReg = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-  const urlReg = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
   switch (field) {
     case "fields": {
       return value.trim() === "";
     }
     case "email": {
       if (emailReg.test(value) === true) {
-        return value.trim() === "";
-      } else {
-        return true;                                       
-      }
-    }
-    case "url":{
-      if (urlReg.test(value) === true) {
         return value.trim() === "";
       } else {
         return true;                                       
