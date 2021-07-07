163bd46d1912c51c78022d399240d0fbe14349a1
Asura <aravind1808@gmail.com>
2021-07-08 00:51:51 +0530

    Adding nginx

--- client/src/components/utils/urls.js
+++ client/src/components/utils/urls.js
@@ -1,7 +1,6 @@
- 
 module.exports = {
-  AUTH_SERVICES:`http://localhost:3005`,
-  CHAT_SERVICES:`http://localhost:3006`
+  AUTH_SERVICES: `http://localhost:3005/api`,
+  CHAT_SERVICES: `http://localhost:3006/api`,
   // AUTH_SERVICES:`https://auth-services-aden.herokuapp.com/`,
   // CHAT_SERVICES:'/'
-}
+};
